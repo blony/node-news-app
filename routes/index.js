e65cfb9fbb805971498f487fa498f22709c0df05
Bobby <h122656227@gmail.com>
2019-06-13 22:32:17 +0800

    update smscode api

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const table = require('./table')
 module.exports = app => {
     //用户操作
     app.post('/user/login',user.login)
-    app.get('/user/info',user.userInfo)
+    app.get('/user/getsmscode',user.getsmscode)
     app.post('/user/logout',user.logout)
 
     //表格数据获取
